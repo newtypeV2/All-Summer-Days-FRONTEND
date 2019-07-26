9d4596a06ea4698af9dacc68ce6ec796e112e0a1
Peter Viss <peter.c.viss@biola.edu>
2019-07-26 11:57:43 -0400

    added pic

--- src/Components/CharacterCard.js
+++ src/Components/CharacterCard.js
@@ -2,25 +2,24 @@ import React, {Component} from 'react';
 import {Card, Col} from 'react-bootstrap';
 const CharacterCard = (props) => {
     
-        return(
-            
-           <Col>
-           <Card style={{width: "15rem"}}>
-            <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-            <Card.Title>{props.character.firstname}</Card.Title>
-            <Card.Text>
-                 This is a wider card with supporting text below as a natural lead-in to
-                 additional content. This content is a little bit longer.
-            </Card.Text>
-        </Card.Body>
-        <Card.Footer>
+    return( 
+      <Col>
+        <Card style={{width: "15rem"}}>
+            <Card.Img variant="top" src="https://image.shutterstock.com/image-vector/default-placeholder-avatar-profile-on-260nw-490458475.jpg" />
+            <Card.Body>
+                <Card.Title>{props.character.firstname}</Card.Title>
+                <Card.Text>
+                    This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.
+                </Card.Text>
+            </Card.Body>
+           <Card.Footer>
             <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-  </Card>
-  </Col>
+           </Card.Footer>
+        </Card>
+      </Col>
         
-        )
+    )
     
 } 
 
