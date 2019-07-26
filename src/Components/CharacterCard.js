fb5ea69621cccc86f3a929864644046ee4d952d1
Peter Viss <peter.c.viss@biola.edu>
2019-07-26 12:54:31 -0400

    Some Func

--- src/Components/CharacterCard.js
+++ src/Components/CharacterCard.js
@@ -8,13 +8,11 @@ const CharacterCard = (props) => {
             <Card.Img variant="top" src="https://image.shutterstock.com/image-vector/default-placeholder-avatar-profile-on-260nw-490458475.jpg" />
             <Card.Body>
                 <Card.Title>{props.character.firstname} {props.character.lastname}</Card.Title>
-                <Card.Text>
                   <ul>
                     <li>Class : {props.character.char_class.name}</li>
                     <li>Level : {props.character.level}</li>
                     <li>Level : {props.character.max_hp}</li>
                   </ul>
-                </Card.Text>
             </Card.Body>
            <Card.Footer>
             <small className="text-muted">Last updated 3 mins ago</small>
--- src/Containers/MainContainer.js
+++ src/Containers/MainContainer.js
@@ -24,26 +24,6 @@ class MainContainer extends Component {
     render(){
         console.log(this.state.allCharacters)
         return(
-<<<<<<< HEAD
-        <div>
-        <Navbar bg={'dark'} variant={'dark'}>
-            <Navbar.Brand href="#home">All Summer Days</Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Collapse className="justify-content-end">
-                <Navbar.Text>
-                    Signed in as: <a href="#login">Peter/Gian</a>
-                </Navbar.Text>
-            </Navbar.Collapse>
-        </Navbar>
-        <div>
-          <Row>
-            <Col sm={7}><CharacterContainer characters={this.state.allCharacters}/></Col>
-            <Col sm={5}>2 of 2</Col>
-          </Row>   
-        </div> 
-            
-        </div>
-=======
           <div>
           <Navbar bg={'dark'} variant={'dark'}>
               <Navbar.Brand href="#home">All Summer Days</Navbar.Brand>
@@ -61,7 +41,6 @@ class MainContainer extends Component {
             </Row>   
           </div> 
           </div>
->>>>>>> 3c30f26d9f638a5cc8171b77e79464a2e09a25d9
         )  
     }
 }
