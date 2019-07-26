d881daa0fc70a380fcee4f653baefa47e1328526
Peter Viss <peter.c.viss@biola.edu>
2019-07-25 21:55:43 -0400

    adding Char Container

---
+++ src/Containers/CharacterContainer.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class CharacterContainer extends Component {
+
+
+
+
+
+    render(){
+        return(){
+        <div>
+            
+        </div>
+        }
+    }
+}
+
+export default CharacterContainer
