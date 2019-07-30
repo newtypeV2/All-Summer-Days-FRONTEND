9d08eae4f8d2c9feebc9c8dc0c1657e5765390a6
newtypeV2 <gianlibunao@gmail.com>
2019-07-30 18:33:37 -0400

    Added Header.

--- src/Components/Navibar.js
+++ src/Components/Navibar.js
@@ -5,6 +5,7 @@ const Navibar = (props) => {
     return(
           <Navbar bg={'dark'} variant={'dark'} className="navibar">
               <Navbar.Brand>All Summer Days</Navbar.Brand>
+              <span className="navibar">by Gian Libunao and Peter Viss with consultations from Matthew Williams</span>
                 <Navbar.Collapse className="justify-content-end">
                 <Navbar.Text>
                 {props.loggedInUser.username ? props.loggedInUser.username : "Login"} 
