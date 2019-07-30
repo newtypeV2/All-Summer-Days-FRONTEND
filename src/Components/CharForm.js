4bd477bf5fe2ca50dc0e280473694e8370f93863
Peter Viss <peter.c.viss@biola.edu>
2019-07-29 22:11:25 -0400

    able to select profs now

--- src/Components/CharForm.js
+++ src/Components/CharForm.js
@@ -3,7 +3,14 @@ import {Form, Col, Card, Row,DropdownButton,Dropdown} from 'react-bootstrap';
 import AbilityScore from './AbilityScore'
 class CharForm extends Component{
     state = {
+        chosen: [],
+        skills: null,
+        instr: null,  
+        tools: null, 
         className: null, 
+        skillNum: 0, 
+        intrNum: 0, 
+        toolNum: 0,
         abilityScore: 27,
         character: {
             firstName: "",
@@ -26,13 +33,18 @@ class CharForm extends Component{
             hair: "",
             background: "",
             alignment: "",
-            proficiency_ids: []
-              
-            
+            proficiency_ids: []  
         }
     }
+        displayProf = () => {
+         return this.state.chosen.map(choose => {
+            return(
+             <li>{choose}</li>
+            )
+          })
+        }
 
-         
+
        pointBuy = (cost, num, props) => {
         let obj = Object.keys(this.state.character)
         let name = obj.filter(key => {if(key === props.name){return key}else{}})
@@ -44,9 +56,9 @@ class CharForm extends Component{
              character: {
                  ...this.state.character,
                  [name]: num + 1
-             }
-        })
-    }
+                }
+               })
+            }
        } 
        
        pointCost = (event, props) => {
@@ -87,37 +99,44 @@ class CharForm extends Component{
             
     getClass = (event) => {
       let name = this.props.classList.filter(list => {if(list.name === event.target.value){return list}})
+      let skill = name[0].choose_proficiencies[0]
+      let instruments = name[0].choose_proficiencies[1] ? name[0].choose_proficiencies[1] : null
+      let tools = name[0].choose_proficiencies[2] ? name[0].choose_proficiencies[2] : null
+      let num1 = skill.slice(-1)[0].choose
+      let num2 = instruments ? instruments.slice(-1)[0].choose : 0
+      let num3 = tools ? tools.slice(-1)[0].choose : 0
       this.setState({
+        skills: skill,
+        instr: instruments,
+        tools: tools, 
+        skillNum: num1,
+        instrNum: num2,
+        toolNum: num3, 
         className: name[0],
         character: {
           ...this.state.character,
           char_class_id: name[0].id
         } 
       })   
+      
     } 
 
-    getClassList = (event) =>{
+    setClassList = (event, theKey, theNum) =>{
+      let keyMe = theKey[0]
       let newestName = this.state.className.choose_proficiencies.map(arr => {
         return arr.filter(prof => {if(prof.name === event.target.value){
           return prof
         }else{return}
         })
-      })
-
-      // let countdown = this.state.className.choose_proficiencies.map(arr => {
-         
-      //    return [...arr , arr.slice(-1)[0].choose = arr.slice(-1)[0].choose - 1]
-        
-      // }
-       
-      // )
-     
+      })   
       let newestArr = newestName.filter(arr => {return arr.length !== 0})
-      
+ 
       if(this.state.character.proficiency_ids.includes(newestArr[0][0].id)){
         alert("Already Selected")
         }else{
           this.setState({
+            chosen: [...this.state.chosen, event.target.value],
+            [keyMe]: theNum,
             className: {...this.state.className},
             character: {
             ...this.state.character,
@@ -128,32 +147,75 @@ class CharForm extends Component{
      }
     
      displaySkillProfs = () =>{
-        return <DropdownButton id="dropdown-basic-button" title="Skill Proficiences">
+      let keys = Object.keys(this.state)
+      let theKey = keys.filter(key => {return key==='skillNum'})
+      let theNum = this.state.skillNum - 1
+       if(this.state.skillNum !== 0){
+        
+        return(<Form.Group controlId="exampleForm.ControlSelect2" onChange={(event) => this.setClassList(event, theKey, theNum)}>
+            <Form.Label style={{color: 'white'}} >Skills to Choose: {this.state.skillNum}</Form.Label>
+            <Form.Control as="select">
+              <option>please choose</option>
           {this.state.className.choose_proficiencies[0].map(prof =>
-            <Dropdown.Item value={prof.id}>{prof.name}</Dropdown.Item>
+            <option value={prof.name}>{prof.name}</option>
           )}
-        </DropdownButton>
+          </Form.Control>
+        </Form.Group>
+        
+        )}else{return }
+        
       }
 
-     displayInstProfs = (skillPoint) => {
+     displayInstProfs = () => {
+      let keys = Object.keys(this.state)
+      let theKey = keys.filter(key => {return key==='instrNum'})
+      let theNum = this.state.instrNum - 1
+      if(this.state.instrNum !== 0){
+        return(<Form.Group controlId="exampleForm.ControlSelect2" onChange={(event) => this.setClassList(event, theKey, theNum)}>
+            <Form.Label style={{color: 'white'}}>Intruments to Choose: {this.state.instrNum}</Form.Label>
+            <Form.Control as="select">
+            <option>please choose</option>
+          {this.state.className.choose_proficiencies[1].map(prof =>
+            <option value={prof.name}>{prof.name}</option>
+          )}
+          </Form.Control>
+        </Form.Group>
+        
+        )}else{return }
+      
+        
+      }
+    
 
+    displayToolsProfs = () => {
+      let keys = Object.keys(this.state)
+      let theKey = keys.filter(key => {return key==='toolNum'})
+      let theNum = this.state.toolNum - 1
+      if(this.state.toolNum !== 0){
+        return(<Form.Group controlId="exampleForm.ControlSelect2" onChange={(event) => this.setClassList(event, theKey, theNum)}>
+            <Form.Label style={{color: 'white'}} >Tools to Choose: {this.state.toolNum}</Form.Label>
+            <Form.Control as="select">
+            <option>please choose</option>
+          {this.state.className.choose_proficiencies[2].map(prof =>
+            <option value={prof.name}>{prof.name}</option>
+          )}
+          </Form.Control>
+        </Form.Group>
+        )}else{return}      
     }
 
-    displayToolsProfs = (skillPoint) => {
-
+    stating = (event) => {
+      let name = event.target.classList[0]
+      this.setState({
+        character: {
+          ...this.state.character,
+        [name]: event.target.value
+        }
+      })
+      
     }
-
 
      render(){
-      
-      if (this.state.className){
-        if(this.state.className.choose_proficiencies.length===3){
-          debugger
-        }else if(this.state.className.choose_proficiencies.length==2){
-          debugger
-        }else{
-        }
-      }
  return (
      <div>
 
@@ -162,11 +224,11 @@ class CharForm extends Component{
         <Col sm={3}>
     <Form.Group controlId="formHorizontalEmail">
     <Form.Label style={{color: 'white'}} >First Name</Form.Label>
-    <Form.Control type="" placeholder="put name first here" />
+    <Form.Control type="" placeholder="put name first here" className="firstName" onChange={this.stating}/>
   </Form.Group>
   <Form.Group controlId="exampleForm.ControlInput1">
     <Form.Label style={{color: 'white'}} >Last Name</Form.Label>
-    <Form.Control type="" placeholder="put name last here" />
+    <Form.Control type="" placeholder="put name last here" className="lastName" onChange={this.stating}/>
   </Form.Group>
   <Form.Group controlId="exampleForm.ControlSelect1" onChange={this.getClass}>
     <Form.Label style={{color: 'white'}} >Character Class</Form.Label>
@@ -200,7 +262,7 @@ class CharForm extends Component{
   <AbilityScore name={'charisma'} abilityScore={this.state.abilityScore} score={this.state.character.charisma} setScore={this.setScore} choice={this.state.choices} buy={this.pointCost}/>
   <Form.Group controlId="exampleForm.ControlSelect2">
     <Form.Label style={{color: 'white'}} >Eye Color</Form.Label>
-    <Form.Control as="select" >
+    <Form.Control as="select" className="eyes" onChange={this.stating}>
       <option>Yellow</option>
       <option>Amber</option>
       <option>Brown</option>
@@ -225,7 +287,7 @@ class CharForm extends Component{
   </Form.Group>
   <Form.Group controlId="exampleForm.ControlSelect2">
     <Form.Label style={{color: 'white'}} >Skin Color</Form.Label>
-    <Form.Control as="select" >
+    <Form.Control as="select" className="skin" onChange={this.stating}>
     <option>Pale</option>
       <option>Fair</option>
       <option>Light</option>
@@ -265,7 +327,7 @@ class CharForm extends Component{
     </Form.Control>
   </Form.Group>
   <Form.Group controlId="exampleForm.ControlSelect2">
-    <Form.Label style={{color: 'black'}} >Hair Color</Form.Label>
+    <Form.Label style={{color: 'black'}} className="hair" onChange={this.stating}>Hair Color</Form.Label>
     <Form.Control as="select" >
     <option>Black</option>
       <option>Gray</option>
@@ -303,11 +365,11 @@ class CharForm extends Component{
   </Form.Group>
   <Form.Group controlId="exampleForm.ControlTextarea1">
     <Form.Label style={{color: 'black'}} >Background</Form.Label>
-    <Form.Control as="textarea" rows="3" />
+    <Form.Control as="textarea" rows="3" className="background" onChange={this.stating}/>
   </Form.Group>
   <Form.Group controlId="exampleForm.ControlSelect2">
     <Form.Label style={{color: 'black'}} >Alignment</Form.Label>
-    <Form.Control as="select" >
+    <Form.Control as="select" className='alignment' onChange={this.stating}>
       <option>Lawful Good</option>
       <option>Neutral Good </option>
       <option>Chaotic Good </option>
@@ -319,8 +381,8 @@ class CharForm extends Component{
     </Form.Control>
   </Form.Group>
   {this.state.className ? this.displaySkillProfs() : null}
-  {this.state.className ? this.displaySkillProfs() : null}
-  {this.state.className ? this.displaySkillProfs() : null}
+  {this.state.className ? this.displayInstProfs() : null}
+  {this.state.className ? this.displayToolsProfs() : null}
   </Col>
 
   <Col sm={3}>
@@ -334,7 +396,7 @@ class CharForm extends Component{
       <Card.Body>
         <Card.Title> Chosen Proficiences:</Card.Title>
         <ul>
-          
+          {this.displayProf()}
         </ul>
       </Card.Body>
     </Card>
