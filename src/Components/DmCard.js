2f37334b9a8e9fd72d2a7b8a6ef4ee098a78796b
Peter Viss <peter.c.viss@biola.edu>
2019-07-31 00:54:21 -0400

    skateboard

---
+++ src/Components/DmCard.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+import {Card, ListGroup, ListGroupItem} from 'react-bootstrap'
+const DmCard = (props) => {
+    return(
+        <Card style={{ width: '18rem' }}>
+            
+  <Card.Body>
+    <Card.Title>{props.char.firstname}</Card.Title>
+  </Card.Body>
+  <ListGroup className="list-group-flush">
+    <ListGroupItem>lvl: {props.char.level}</ListGroupItem>
+    <ListGroupItem>strength: {props.char.strength}</ListGroupItem>
+    <ListGroupItem>dexterity: {props.char.dexterity}</ListGroupItem>
+    <ListGroupItem>constitution: {props.char.constitution}</ListGroupItem>
+    <ListGroupItem>intelligence: {props.char.intelligence}</ListGroupItem>
+    <ListGroupItem>wisdom: {props.char.wisdom}</ListGroupItem>
+    <ListGroupItem>charisma: {props.char.charisma}</ListGroupItem>
+    <ListGroupItem>hitpoints: {props.char.hitpoints}</ListGroupItem>
+    <ListGroupItem>max hp: {props.char.max_hp}</ListGroupItem>
+  </ListGroup>
+
+</Card>
+    )
+}
+export default DmCard
--- src/Containers/DmContainer.js
+++ src/Containers/DmContainer.js
@@ -1,17 +1,38 @@
 import React, {Component} from 'react'
+import DmCard from '../Components/DmCard'
+import {Row, Col, Card} from 'react-bootstrap'
 const campaignUrl = `http://${window.location.hostname}:5000/campaigns`
 class DmContainer extends Component{
-    
+    state = {
+        campaign: {}
+    }
     componentDidMount(){
         fetch(`${campaignUrl}/${this.props.camp.id}`)
         .then(resp => resp.json())
-        .then(data => console.log(data))
+        .then(data => this.setState({
+            campaign: data
+        }))
     }
    
+    getCard = () => {
+        if(this.state.campaign.characters !== undefined){
+       return this.state.campaign.characters.map(char => {
+           return <DmCard key={char.id} char={char}/>
+        })}
+        else{return}
+    }
+
     render(){
         return (
             <div>
-
+            <Row>
+                <Col>
+                <h1 style={{color: 'white'}}>{this.state.campaign.title}</h1>
+                {/* </Col>
+                <Col> */}
+                {this.getCard()}
+                </Col>
+            </Row>
             </div>
         )
     }
